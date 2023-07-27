e660bc461128c2e5870bd62ce25bf2d4058ed2dd
vic <118995708+vwanako@users.noreply.github.com>
2023-07-27 20:06:08 -0300

    updates

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include "Users.h"
 
+// void userMenu()
+// {
+//     if ()
+// }
+
 int main()
 {
     // admin newAdmin("john doe", "johndoeadmin", "1234", "johndoe@mail.com");
